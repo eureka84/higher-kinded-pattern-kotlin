aa2959d2f566ba0e0647e29be4536e2865a6c42a
eureka84 <angelosciarra@ymail.com>
2019-05-05 22:42:56 +0200

    minor refactor

--- src/main/kotlin/com/eureka/lib/Num.kt
+++ src/main/kotlin/com/eureka/lib/Num.kt
@@ -8,28 +8,28 @@ interface Num<N> {
     operator fun N.div(other: N): N
 
     companion object {
-        fun int(): Num<Int> = object : Num<Int> {
+        val int: Num<Int> = object : Num<Int> {
             override fun Int.plus(other: Int): Int = this + other
             override fun Int.minus(other: Int): Int = this - other
             override fun Int.times(other: Int): Int = this * other
             override fun Int.div(other: Int): Int = this / other
         }
 
-        fun long(): Num<Long> = object : Num<Long> {
+        val long: Num<Long> = object : Num<Long> {
             override fun Long.plus(other: Long): Long = this + other
             override fun Long.minus(other: Long): Long = this - other
             override fun Long.times(other: Long): Long = this * other
             override fun Long.div(other: Long): Long = this / other
         }
 
-        fun float(): Num<Float> = object : Num<Float> {
+        val float: Num<Float> = object : Num<Float> {
             override fun Float.plus(other: Float): Float = this + other
             override fun Float.minus(other: Float): Float = this - other
             override fun Float.times(other: Float): Float = this * other
             override fun Float.div(other: Float): Float = this / other
         }
 
-        fun double(): Num<Double> = object : Num<Double> {
+        val double: Num<Double> = object : Num<Double> {
             override fun Double.plus(other: Double): Double = this + other
             override fun Double.minus(other: Double): Double = this - other
             override fun Double.times(other: Double): Double = this * other
--- src/test/kotlin/com/eureka/FunctorUsageTest.kt
+++ src/test/kotlin/com/eureka/FunctorUsageTest.kt
@@ -10,24 +10,24 @@ class FunctorUsageTest {
 
     @Test
     fun `arithmetic with option and ints`() {
-        val arithmetic = object : ArithmeticWithEffects<ForOption, Int> {
+        val program = object : ArithmeticWithEffects<ForOption, Int> {
             override val f: Functor<ForOption> = Option.functor
-            override val n: Num<Int> = Num.int()
+            override val n: Num<Int> = Num.int
         }
 
-        val result = arithmetic.square(Option.just(3)).fix()
+        val result: Option<Int> = program.square(Option.just(3)).fix()
 
         assertThat(result, equalTo(Option.just(9)))
     }
 
     @Test
     fun `arithmetic with Try and double`() {
-        val arithmetic = object : ArithmeticWithEffects<ForTry, Double> {
+        val program = object : ArithmeticWithEffects<ForTry, Double> {
             override val f: Functor<ForTry> = Try.functor
-            override val n: Num<Double> = Num.double()
+            override val n: Num<Double> = Num.double
         }
 
-        val result = arithmetic.square(Try.attempt { 3.0 }).fix()
+        val result = program.square(Try.attempt { 3.0 }).fix()
 
         assertThat(result, equalTo(Try.attempt { 9.0 }))
     }
